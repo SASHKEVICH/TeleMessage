78ebd2c07263be1b9e1d4604de37efc3533b7660
Aleksander Bekrenev <praise2001@yandex.ru>
2022-01-01 19:40:18 +0700

    добавлен prism

---
+++ Server/SocketsManager/ConnectionManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server.SocketsManager
+{
+    public class ConnectionManager
+    {
+        private ConcurrentDictionary<Guid, WebSocket> _connections = new ConcurrentDictionary<Guid, WebSocket>();
+
+        public WebSocket GetSocketById(Guid id)
+        {
+            return _connections.FirstOrDefault(x => x.Key == id).Value;
+        }
+
+        public ConcurrentDictionary<Guid, WebSocket> GetAllConnections()
+        {
+            return _connections;
+        }
+
+        public Guid GetId(WebSocket socket)
+        {
+            return _connections.FirstOrDefault(x => x.Value == socket).Key;
+        }
+
+        public async Task RemoveSocketAsync(Guid id)
+        {
+            _connections.TryRemove(id, out var socket);
+            await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Connection Closed", CancellationToken.None);
+        }
+
+        public void AddSocket(WebSocket socket)
+        {
+            _connections.TryAdd(GetConnectionId(), socket);
+        }
+
+        private Guid GetConnectionId()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
---
+++ Server/SocketsManager/SocketMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace Server.SocketsManager
+{
+    public class SocketMiddleware
+    {
+        private readonly RequestDelegate _next;
+        
+        public SocketHandler Handler { get; set; }
+
+        public SocketMiddleware(RequestDelegate next, SocketHandler handler)
+        {
+            this._next = next;
+            this.Handler = handler;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.WebSockets.IsWebSocketRequest)
+            {
+                return;
+            }
+
+            var socket = await context.WebSockets.AcceptWebSocketAsync();
+
+            await Handler.OnConnected(socket);
+
+            await Receive(socket, async (result, buffer) =>
+            {
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    await Handler.Receive(socket, result, buffer);
+                }
+                else if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await Handler.OnDisconnected(socket);
+                }
+            });
+        }
+
+        private async Task Receive(WebSocket socket, Action<WebSocketReceiveResult, byte[]> messageHandler)
+        {
+            var buffer = new byte[1024 * 4];
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                messageHandler(result, buffer);
+            }
+        }
+        
+        
+    }
+}
