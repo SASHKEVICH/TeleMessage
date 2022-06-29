9146357b1322473018ce177c01526109164b3f3d
Aleksander Bekrenev <praise2001@yandex.ru>
2022-06-29 22:22:01 +0700

    feat: added service to run server

---
+++ Server.Service/Installer1.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace Server.Service
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+
+            var serviceInstaller = new ServiceInstaller();
+            var processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalService;
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "Run_Telemessage_server_service";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
---
+++ Server.Service/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+
+namespace Server.Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        private ProcessStartInfo _processInfo;
+        private Process _process;
+
+        public Service1()
+        {
+            InitializeComponent();
+            CanStop = true;
+            CanPauseAndContinue = true;
+
+            SetupProcess();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _process = Process.Start(_processInfo);
+        }
+
+        protected override void OnStop()
+        {
+            _process.Kill();
+            _process = null;
+            _processInfo = null;
+        }
+
+        private void SetupProcess()
+        {
+            var serverProcessPath = Path.Combine(Directory.GetCurrentDirectory(), "Server.exe");
+            _processInfo = new ProcessStartInfo(serverProcessPath);
+            _processInfo.UseShellExecute = false;
+            _processInfo.RedirectStandardOutput = true;
+            _processInfo.RedirectStandardError = true;
+            _processInfo.RedirectStandardInput = true;
+            _processInfo.CreateNoWindow = true;
+            _processInfo.ErrorDialog = false;
+            _processInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        }
+    }
+}
