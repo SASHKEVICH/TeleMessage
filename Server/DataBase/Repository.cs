505014a01fb3f0f733b9dc928ab36cbda7ad9daf
Aleksander Bekrenev <praise2001@yandex.ru>
2022-07-01 21:26:05 +0700

    feat: repository methods expanded

--- Server/DataBase/Repository.cs
+++ Server/DataBase/Repository.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Core;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Server.ApplicationContext;
 
 namespace Server.DataBase
@@ -28,34 +28,75 @@ namespace Server.DataBase
 
         #region Methods
 
-        public IEnumerable<Message> GetMessageList()
+        #region MessageMethods
+
+        public IEnumerable<Message> GetMessages()
         {
             return _dataBaseContext.Messages;
         }
 
-        public Message GetMessage(int id)
+        public Message GetMessage(Guid id)
         {
             return _dataBaseContext.Messages.Find(id);
         }
 
-        public void Create(Message message)
+        public void CreateMessage(Message message)
         {
             _dataBaseContext.Messages.Add(message);
         }
 
-        public void Update(Message message)
+        public void UpdateMessage(Message message)
         {
             _dataBaseContext.Entry(message).State = EntityState.Modified;
         }
 
-        public void Delete(int id)
+        public void DeleteMessage(Guid id)
         {
             var message = _dataBaseContext.Messages.Find(id);
             if (message != null)
             {
-                _dataBaseContext.Messages.Remove(message); 
+                _dataBaseContext.Remove(message);
             }
         }
+        
+        #endregion
+
+        #region UserMethods
+        
+        public List<User> GetUsers(List<Guid> guids)
+        {
+            return guids.Select(guid => GetUser(guid)).ToList();
+        }
+
+        public User GetUser(Guid id)
+        {
+            return _dataBaseContext.Users.Find(id);
+        }
+
+        public void CreateUser(User user)
+        {
+            var findingUser = GetUser(user.UserId);
+
+            if (findingUser == null) return;
+            
+            _dataBaseContext.Users.Add(user);
+        }
+
+        public void UpdateUser(User user)
+        {
+            _dataBaseContext.Entry(user).State = EntityState.Modified;
+        }
+
+        public void DeleteUser(Guid id)
+        {
+            var user = _dataBaseContext.Users.Find(id);
+            if (user != null)
+            {
+                _dataBaseContext.Remove(user);
+            }
+        }
+
+        #endregion
 
         public void Save()
         {
