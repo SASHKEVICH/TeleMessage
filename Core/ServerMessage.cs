23ad746c4869db01714cb6e422c044ee6bbc7182
Aleksander Bekrenev <praise2001@yandex.ru>
2022-07-01 21:26:32 +0700

    feat: new DTO

---
+++ Core/ServerMessage.cs
@@ -0,0 +1,11 @@
+namespace Core
+{
+    public class ServerMessage
+    {
+        public List<Message> InitialMessages { get; set; }
+        public List<User> ConnectedUsers { get; set; }
+        public Message Message { get; set; }
+        public MessageType Type { get; set; }
+    }
+}
+
